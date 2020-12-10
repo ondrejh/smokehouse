cf6bac14ff44f74d75035814e3e1a11175f84130
Ondrej Hejda <ondrejh.ck@email.cz>
2020-12-10 21:33:44 +0100

    in (slow) progress

--- webi/data_input.php
+++ webi/data_input.php
@@ -1,11 +1,27 @@
 <?php
+
+define("NODE", "udirna");
+define("PSWD", "1234");
+
+define("DB_FILE", "test.db")
+define("NODE_TABLE", "nodes")
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$node = $_POST['node'];
+	$pswd = $_POST['passwd'];
 	$data = $_POST['data'];
-	if (empty($data)) {
-		echo "ERROR: NO DATA";
-	} else {
-		// use data
-		echo "OK";
+	if (($node != NODE) && ($pswd != PSWD)) {
+		echo "ERROR: INCORRECT CREDENTIALS";
+	}
+	else {
+		if (empty($data)) {
+			echo "ERROR: NO DATA";
+		} else {
+			$db = new SQLite3(DB_FILE);
+			
+			// use data
+			echo "OK";
+		}
 	}
 }
 else echo "ERROR: NO POST";
