06afa3923608d8495ee47f811e52320a0df7bae4
Ondrej Hejda <ondrejh.ck@email.cz>
2020-12-16 14:12:38 +0100

    allow change device name

--- webi/list_devices.php
+++ webi/list_devices.php
@@ -22,31 +22,39 @@
           echo "<p>Neplatné kombinace jména a hesla !!!</p>". PHP_EOL;
         }
         else {
-            $conn = new mysqli(SERVER, USER, PASWD, DBNAME);
+          $conn = new mysqli(SERVER, USER, PASWD, DBNAME);
             
-            $query = "SELECT nick FROM users WHERE id = '". $userid. "';";
-            $res = $conn->query($query);
-            $row = $res->fetch_row();
-            $nick = $row[0];
-            echo "<h2>Zařízení uživatele ". $nick. "</h2>". PHP_EOL;
+          $query = "SELECT nick FROM users WHERE id = '". $userid. "';";
+          $res = $conn->query($query);
+          $row = $res->fetch_row();
+          $nick = $row[0];
+          echo "<h2>Zařízení uživatele ". $nick. "</h2>". PHP_EOL;
             
-            $query = "SELECT devices.name, devices.idstr, types.description, devices.keystr FROM devices INNER JOIN types ON devices.typeid=types.id WHERE userid = '". $userid. "';";
-            $res = $conn->query($query);
-            if ($res->num_rows > 0) {
-                echo "<table>". PHP_EOL. "<tr><th>Jméno</th><th>Typ</th><th>Klíč</th><th>Aktivní</th></tr>". PHP_EOL;
-                while ($row = $res->fetch_assoc()) {
-                    $dev_name = $row['name'];
-                    $dev_type = $row['description'];
-                    $dev_key = $row['keystr'];
-                    $dev_active = ($row['idstr'] == null) ? 'NE' : 'ANO';
-                    echo "<tr><td>". $dev_name. "</td><td>". $dev_type. "</td><td>". $dev_key. "</td><td>". $dev_active. "</td></tr>". PHP_EOL;
-                }
-                echo "<table>". PHP_EOL;
+          if (( isset($_POST['action']) ) and ( $_POST['action'] == 'change_name' )) {
+            $query = "UPDATE devices SET name = '". $_POST['dev_name']. "' WHERE id = ". $_POST['dev_id']. ";";
+            $conn->query($query);  
+          }
+            
+          $query = "SELECT devices.id, devices.name, devices.idstr, types.description, devices.keystr FROM devices INNER JOIN types ON devices.typeid=types.id WHERE userid = '". $userid. "';";
+          #echo PHP_EOL. $query. PHP_EOL;
+          $res = $conn->query($query);
+          if ($res->num_rows > 0) {
+            echo "<table>". PHP_EOL. "<tr><th>Jméno</th><th>Typ</th><th>Klíč</th><th>Aktivní</th></tr>". PHP_EOL;
+            while ($row = $res->fetch_assoc()) {
+              $dev_id = $row['id'];
+              $dev_name = $row['name'];
+              $dev_type = $row['description'];
+              $dev_key = $row['keystr'];
+              $dev_active = ($row['idstr'] == null) ? 'NE' : 'ANO';
+              $edit_form = "<form method='post' action='". $_SERVER['PHP_SELF']. "'><input type='hidden' name='user' value='". $user. "'><input type='hidden' name='password' value='". $pwd. "'><input type='hidden' name='dev_id' value='". $dev_id. "'><input type='hidden' name='action' value='change_name'><input type='text' name='dev_name' value = '". $dev_name. "'><input type='submit' value='Upravit'></form>";
+              echo "<tr><td>". $edit_form. "</td><td>". $dev_type. "</td><td>". $dev_key. "</td><td>". $dev_active. "</td></tr>". PHP_EOL;
             }
-            else {
-                echo "<p>Nic. Milý ". $nick. " měl by ses polepšit.</p>". PHP_EOL;
-            }
-            $conn->close();
+            echo "<table>". PHP_EOL;
+          }
+          else {
+            echo "<p>Nic. Milý ". $nick. " měl by ses polepšit.</p>". PHP_EOL;
+          }
+          $conn->close();
         }
       }
       
