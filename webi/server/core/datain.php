2b35153ce28a1fdbfafc5c7713ace637cf0ba760
Ondřej Hejda <ondrejh.ck@email.cz>
2023-03-11 13:25:40 +0100

    keg scale device data input

--- webi/server/core/datain.php
+++ webi/server/core/datain.php
@@ -16,47 +16,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $res = $conn->query($query);
 
   if ($res->num_rows > 0) {
-	$row = $res->fetch_row();
-	$dev_id = $row[0];
-	$dev_idstr = $row[1];
-	$dev_type = $row[2];
+		$row = $res->fetch_row();
+		$dev_id = $row[0];
+		$dev_idstr = $row[1];
+		$dev_type = $row[2];
 	  
-	if ($dev_type == 't_simple') {
-	  $query = "CREATE TABLE IF NOT EXISTS dev_". $keystr. "(id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, temp REAL, tstmp DATETIME DEFAULT CURRENT_TIMESTAMP);";
-	  $conn->query($query);
-	  $query = "INSERT INTO dev_". $keystr. "(temp) VALUES (". $data. ");";
-	  $conn->query($query);
-	  if ($dev_idstr == null) {
-		$query = "UPDATE devices SET idstr = '". $idstr. "' WHERE id = ". $dev_id. ";";
-		$conn->query($query);
-	  }
-	  $conn->close();
-	  echo "OK";
-    }
-    else if ($dev_type == 'th_simple') {
-    }
-    else if ($dev_type == 't_double') {
+		if ($dev_type == 't_simple') {
+	  	$query = "CREATE TABLE IF NOT EXISTS dev_". $keystr. "(id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, temp REAL, tstmp DATETIME DEFAULT CURRENT_TIMESTAMP);";
+	  	$conn->query($query);
+	  	$query = "INSERT INTO dev_". $keystr. "(temp) VALUES (". $data. ");";
+	  	$conn->query($query);
+	  	if ($dev_idstr == null) {
+				$query = "UPDATE devices SET idstr = '". $idstr. "' WHERE id = ". $dev_id. ";";
+				$conn->query($query);
+	  	}
+	  	$conn->close();
+	  	echo "OK";
+		}
+		else if ($dev_type == 'th_simple') {
+		}
+		else if ($dev_type == 't_double') {
     	$tab_name = "dev_". $keystr;
-	$query = "CREATE TABLE IF NOT EXISTS ". $tab_name. "(id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, temp1 REAL, temp2 REAL, tstmp DATETIME DEFAULT CURRENT_TIMESTAMP);";
-	$conn->query($query);
-	$id = get_update_id($tab_name, 300, $conn);
-	if ($id)
-		$query = "UPDATE ". $tab_name. " SET tstmp = NOW(), temp1 = ". $data[0].", temp2 = ". $data[1]. " WHERE id = ". $id. ";";
-	else
-		$query = "INSERT INTO ". $tab_name. "(temp1, temp2) VALUES (". $data[0]. ", ". $data[1]. ");";
-	$conn->query($query);
-	if ($dev_idstr == null) {
-		$query = "UPDATE devices SET idstr = '". $idstr. "' WHERE id = ". $dev_id. ";";
-		$conn->query($query);
+			$query = "CREATE TABLE IF NOT EXISTS ". $tab_name. "(id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, temp1 REAL, temp2 REAL, tstmp DATETIME DEFAULT CURRENT_TIMESTAMP);";
+			$conn->query($query);
+			$id = get_update_id($tab_name, 300, $conn);
+			if ($id)
+				$query = "UPDATE ". $tab_name. " SET tstmp = NOW(), temp1 = ". $data[0].", temp2 = ". $data[1]. " WHERE id = ". $id. ";";
+			else
+				$query = "INSERT INTO ". $tab_name. "(temp1, temp2) VALUES (". $data[0]. ", ". $data[1]. ");";
+			$conn->query($query);
+			if ($dev_idstr == null) {
+				$query = "UPDATE devices SET idstr = '". $idstr. "' WHERE id = ". $dev_id. ";";
+				$conn->query($query);
+			}
+			limitTableCount($tab_name, "tstmp", TABLE_LIMIT, $conn);
+			$conn->close();
+			echo "OK";
+		}
+		else if ($dev_type == 'keg_scale' ) {
+			if ($dev_idstr == null) {
+				$query = "UPDATE devices SET idstr = '". $idstr. "' WHERE id = ". $dev_id. ";";
+				$conn->query($query);
+			}
+			$tab_name = "dev_". $keystr;
+    	$query = "CREATE TABLE IF NOT EXISTS ". $tab_name. "(name VARCHAR(32), whg REAL, brs REAL, vol REAL, tstmp DATETIME DEFAULT CURRENT_TIMESTAMP);";
+			$conn->query($query);
+			$query = "SELECT * FROM ". $tab_name. ";";
+			$res = $conn->query($query);
+			if ($res->num_rows > 0)
+    		$query = "UPDATE ". $tab_name. " SET name=\"". $data[0]. "\", whg=". $data[1]. ", brs=". $data[2]. ", vol=". $data[3]. ", tstmp=CURRENT_TIMESTAMP;";
+    	else
+    		$query = "INSERT INTO ". $tab_name. "(name, whg, brs, vol) VALUES (\"". $data[0]. "\", ". $data[1]. ", ". $data[2]. ", ". $data[3]. ");";
+    	$conn->query($query);
+    	$conn->close();
+    	echo "OK";
+		}
 	}
-	limitTableCount($tab_name, "tstmp", TABLE_LIMIT, $conn);
-	$conn->close();
-	echo "OK";
-    }
-    else {
-      echo "ERROR: Device not recognised"; 
-    }
-  }  
+  else {
+  	echo "ERROR: Device not recognised"; 
+  }
 }
 else echo "ERROR: NO POST";
 ?>
